def917c695df1daeb64c2f26a607415575dfb1a4
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2023-12-12 17:39:50 +0000

    back to testing azure ci/cd

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from myapp import create_app
 app = create_app()
 
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(host="0.0.0.0", port=8080, debug=True)
+# if __name__ == '__main__':
+#     app = create_app()
+#     app.run(host="0.0.0.0", port=8080, debug=True)
 
 
 # Path: api/myapp/__init__.py
